9db1666f6e4bc5f4c6550f76c2fd9ee2982a8ba4
slowpoke <proxypoke@lavabit.com>
2012-01-18 00:31:59 +0000

    Added some things to xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -2,15 +2,24 @@ import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
 import XMonad.Layout.NoBorders
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
 main = do
-    xmonad $ defaultConfig
-        { terminal    = "terminator"
-        , layoutHook = smartBorders $ layoutHook defaultConfig
-        , borderWidth = 1
-        }
-        `additionalKeysP`
-        [ ("M-p", spawn "dmenu_run -nb black -nf green -sb green -sf black")
-        , ("M-f", spawn "nightly")
-        , ("M-s", spawn "sleep 0.2; scrot -se 'mv $f ~/screenshots'")
-        ]
+	xmobar <- spawnPipe "/usr/bin/xmobar $HOME/.xmobarrc"
+	xmonad $ defaultConfig
+		{ terminal	= "terminator"
+		, layoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig
+		, logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmobar
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+		, borderWidth = 1
+		}
+		`additionalKeysP`
+		[ ("M-p", spawn "dmenu_run -nb black -nf green -sb green -sf black")
+		, ("M-f", spawn "nightly")
+		, ("M-s", spawn "sleep 0.2; scrot -e 'mv $f ~/screenshots'")
+		]
